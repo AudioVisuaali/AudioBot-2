8bbf930d61d2834f9ea479b7a94cf371e7eebe43
Audiovisuaali <aleksi.vuorio@yahoo.com>
2018-06-25 23:18:19 +0300

    Update start.py

--- start.py
+++ start.py
@@ -2,10 +2,29 @@ from main import Bot
 
 import logging, sys
 
-# logging
-# LOG_FILENAME = 'bot.log'
-# logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
-# logging.exception("message")
+# Setup logging
+#log_level = getattr(logging, )
+#log_format = config.LOG_FORMAT
+
+# Logger
+#logger = logging.getLogger('audiobot')
+#logger.setLevel(log_level)
+
+# Format
+#log_formatter = logging.Formatter(log_format)
+
+#log_console_handle = logging.StreamHandler()
+#log_console_handle.setLevel(log_level)
+#log_console_handle.setFormatter(log_formatter)
+#logger.addHandler(log_console_handle)
+
+#if config.LOG_FILE_PATH:
+#    log_file_handle = logging.FileHandler()
+#    log_file_handle.setLevel(log_level)
+#    log_file_handle.setFormatter(log_formatter)
+#    logger.addHandler(log_file_handle)
+
+
 def main():
     DiscordBot = Bot()
     DiscordBot.start_bot()
